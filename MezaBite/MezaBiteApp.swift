5ef3fbd614cbd8833efe6b46085c2f32603babd7
Viesturs <viesturs.karlivans@gmail.com>
2026-04-14 11:05:36 +0300

    Initial Commit

---
+++ MezaBite/MezaBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MezaBiteApp.swift
+//  MezaBite
+//
+//  Created by Viesturs Karlivans on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MezaBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
